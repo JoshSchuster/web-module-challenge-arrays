4bfe3ba71cf36d22469af11332bd78fa117e143d
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-09-24 19:24:29 -0500

    completes task 7

--- index.js
+++ index.js
@@ -166,6 +166,13 @@ function copy(newArray, originalArray){
 const newArray = [];
 console.log(copy(newArray, originalFlavors)); //test
 
+// function copy(newArray, originalArray){
+
+//     newArray = originalArray.slice(0);
+//     return newArray;
+// }
+
+
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
 Your function should accept: 
@@ -181,13 +188,18 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
+function filterByWord(arraySearching, flavor){
 
-    /*code here*/
-
+    const returnArray = [];
+    for(let i = 0; i < arraySearching.length; i++){
+        if(arraySearching[i].includes(flavor)){
+            returnArray.push(arraySearching[i]);
+        }
+    }
+    return returnArray;
 }
 
-
+console.log(filterByWord(originalFlavors, "Chocolate")); //test
 
 /* 🧁🍦🍨 STRETCH 🍨🍦🍫*/ 
 
